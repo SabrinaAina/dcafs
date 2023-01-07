964a945004082fe9e1d7d2210aec28688a24840c
SabrinaAina <116191769+SabrinaAina@users.noreply.github.com>
2023-01-08 00:58:50 +0800

    Feedback Activity

---
+++ app/src/main/java/ftmk/bitp3453/dcafs/Feedback.java
@@ -0,0 +1,14 @@
+package ftmk.bitp3453.dcafs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
